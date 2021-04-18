ce63b5fe24b9351014692f2b61b33b181b95d348
Ryan Dawkes <ryandawkes@HOR-OSX17.local>
2021-04-18 11:33:55 +0100

    refine dropdown functionality for category

--- pub/js/portfolio.js
+++ pub/js/portfolio.js
@@ -1,7 +1,11 @@
 // Get art from API
-const fetchData = () => {
+const fetchData = (item, itemsArr) => {
+  
   $("#art_container").append(`
-    <div class="art_box d-flex justify-content-center align-items-center col-6 col-md-4 col-xl-3">\
+    <div 
+      class="art_box d-flex justify-content-center align-items-center col-6 col-md-4 col-xl-3" 
+      id="art-${item.id}"
+    >\
       <div class="art_inner d-flex flex-column col-12 col-xl-10 px-0">
         <div class="image_container d-flex justify-content-center align-items-center col-11 m-auto">\
           <img class="art_image" src="${item.image}" alt="${item.title}" />\
@@ -28,50 +32,47 @@ const fetchData = () => {
   `);
 };
 
-const filterChange = (filter) => {
+const filterChange = (category, style) => {
+  let itemsArr = [];
+  let myItems = [];
+
+  myItems.map(item => console.log(item));
+
   fetch("/data").then((res) => {
     res.json().then((data) => {
-      data.map((item) => {
-        filter.on("change", (e) => {
-          const targetValue = e.target.value;
-          console.log(targetValue);
-          if (targetValue == 'all') {
-            fetchData();
-          } else if (targetValue == item.category.toLowerCase()) {
-            $("#art_container").append(`
-              <div class="art_box d-flex justify-content-center align-items-center col-6 col-md-4 col-xl-3">\
-                <div class="art_inner d-flex flex-column col-12 col-xl-10 px-0">
-                  <div class="image_container d-flex justify-content-center align-items-center col-11 m-auto">\
-                    <img class="art_image" src="${item.image}" alt="${item.title}" />\
-                  </div>\
-    
-                  <footer class="d-flex flex-column justify-content-center">\
-                    <div>\
-                      <h3 class="mb-0">${item.title}</h3>\
-                    </div>\
-                    <div class="d-flex justify-content-between">\
-                      <small>${item.category}</small>\
-                      <span class="d-none d-lg-block"><em>${item.style}</em></span>\
-                    </div>\
-                  </footer>\
-    
-                  <div class="d-none">\
-                    <span>${item.id}</span>\
-                    <span>${item.etsy}</span>\
-                    <span>${item.description}</span>\
-                    <span>${item.style}</span>\
-                  </div>\
-                </div>
-              </div>\
-            `);
-          }
-        });
+
+      // Display all on load
+      if (category.val() == 'all') data.map(item => {
+        fetchData(item, myItems);
       });
+
+      // On filter change
+      category.on('change', e => {
+        
+        // Remove items and empty array
+        $('.art_box').remove();
+        itemsArr.splice(0, itemsArr.length);
+        myItems.splice(0, myItems.length);
+
+        const targetValue = e.target.value;
+
+        // If filter has different value, show specified category
+        for (let i = 0; i < data.length; i++) {
+          if (targetValue == data[i].category.toLowerCase()) {
+            myItems.push(data[i]);
+            fetchData(data[i], myItems);
+          } else if (targetValue == 'all') {
+            myItems.push(data[i])
+            fetchData(data[i], myItems);
+          }
+        }
+      })
     });
   });
 };
 
 $(document).ready(() => {
   const category = $("#category_selection");
+  const style = $("#style_selection");
   filterChange(category);
 });
