eb9d2fc500a1d3a2bcb902b6ba629b38065c3097
Ryan Dawkes <rd@moose.co>
2021-04-25 21:42:15 +0100

    change path routing for prod

--- src/app.js
+++ src/app.js
@@ -63,10 +63,10 @@ app.get('/data', (req, res) => {
 })
 
 if (process.env.NODE_ENV == 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     })
 }
 
